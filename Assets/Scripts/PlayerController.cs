85e2b0f7c99521f5cfa8bc5c4bfb3687f1147dc1
dantereto <2247@holbertonschool.com>
2021-12-22 15:51:00 -0500

    goal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -50,6 +50,10 @@ public class PlayerController : MonoBehaviour
             health -= 1;
             Debug.Log("Health: " + health);
         }
+        if (other.gameObject.tag == "Goal")
+        {
+            Debug.Log("You win!");
+        }
     }
 
 }
