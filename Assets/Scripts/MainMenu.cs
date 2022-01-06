ef9f7bf8c665bdaec809b2003875cdffa3094931
dantereto <2247@holbertonschool.com>
2022-01-05 20:42:59 -0500

    final game

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    public Material trapMat;
+    public Material goalMat;
+    public Toggle colorblindMode;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,16 @@ public class MainMenu : MonoBehaviour
     public void PlayMaze()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (colorblindMode.isOn == true)
+        {
+            trapMat.color = new Color32(255, 112, 0, 1);
+            goalMat.color = Color.blue;
+        }
+        else 
+        {
+            trapMat.color = Color.red;
+            goalMat.color = Color.green;
+        }
     }
     public void QuitMaze()
     {
